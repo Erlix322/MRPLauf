2cab79e49296a6158a63c3f9927e095760621d32
Rüdiger von der Hohenlohe <ericbrandt91@gmail.com>
2017-09-19 16:39:06 +0200

    Streams eingefügt

--- mrplauf/src/main/java/org/brandt/mrplauf/RestController/AuftragController.java
+++ mrplauf/src/main/java/org/brandt/mrplauf/RestController/AuftragController.java
@@ -1,10 +1,12 @@
 package org.brandt.mrplauf.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.brandt.mrplauf.entities.Produktionsauftrag;
 import org.brandt.mrplauf.repositories.ProduktionsAuftragRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +16,20 @@ public class AuftragController {
 	@Autowired
 	ProduktionsAuftragRepository auftraege;
 	
-	@RequestMapping("/")
-	public Iterable<Produktionsauftrag> getProduktionsauftraege() {
-		return auftraege.findAll();
+	@RequestMapping("/aufträge/{id}")
+	public Iterable<Produktionsauftrag> getProduktionsauftraege(@PathVariable("id") int id) {
+		
+		List<Produktionsauftrag> list = (List<Produktionsauftrag>) auftraege.findAll();
+		return 
+				
+		list.stream()
+		.filter(x -> x.getID() == id)
+		.collect(Collectors.toList());
+		
+		
+		
+		
+		
 		
 	}
 }
